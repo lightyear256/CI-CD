7f9b16a88c337c33732c80114feca86ebd523106
lightyear256 <157786533+lightyear256@users.noreply.github.com>
2025-10-07 02:28:07 +0530

    day-1 change in the staging

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ export default async function Home() {
   const user= await client.user.findFirst();
   return (
     <div>
+      <div>ok so this was it</div>
       <div>Username: {user?.username}</div>
       <div>Password: {user?.password}</div>
     </div>
